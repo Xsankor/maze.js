55c751a17b7055cbe39c726bd8bc48ce64c5a106
Xsankor <darkpolzovatel@gmail.com>
2020-07-07 16:47:37 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -101,7 +101,7 @@ function checkCollision(){
 }
 
 
-canvas.onclick = coordinats;
+context.onclick = coordinats;
 function coordinats(event){
 let crd = `${event.clientX}:${event.clientY}`;
 console.log(crd);
