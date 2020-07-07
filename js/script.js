bb37818656bd9df57ddc6d56fc642b1ebd833949
Xsankor <darkpolzovatel@gmail.com>
2020-07-07 17:22:34 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -75,7 +75,7 @@ function redraw(){
 
  }
  
- if (y >= 488 && x >= 260) {
+ if (y >= 490 && x >= 260) {
         canvas.style.display = 'none';
         reward.style.display = 'block';
         dy = -3;
